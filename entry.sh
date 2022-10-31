516ec43353f0174331f146c6227f67e72ec0c136
Galyfray <galyfray@gmail.com>
2022-10-31 10:01:27 +0100

    made docker work

--- entry.sh
+++ entry.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
-mkdir -p ./artifacts
-mkdir -p /tmp/docs
+if [ -n "$1" ];then
+  base_dir=${1}
+  if [ "${base_dir: -1}" = "/" ];then 
+    base_dir="${base_dir::-1}"
+  fi 
+  if [ "${base_dir:: 1}" = "." ];then 
+    base_dir="$(pwd)/${base_dir}"
+  fi
+else
+  base_dir="$(pwd)"
+fi
+
 wd=$(pwd)
-inotifywait -e close_write,moved_to,create -m . |
+mkdir -p "${base_dir}/artifacts"
+mkdir -p /tmp/docs
+
+echo ${base_dir}
+
+inotifywait -e close_write,moved_to,create -m ${base_dir} |
 while read -r directory events filename; do
   if [[ "$filename" =~ ^.*\.md$ ]]; then
     base=${filename::-3}
+    full_name="${base_dir}/${filename}"
+    echo ${full_name}
     parsed="/tmp/docs/${base}_parsed.md"
-    out="$(pwd)/artifacts/${base}.pdf"
-    mmdc -e pdf -i $filename -o $parsed &&
+    out="${base_dir}/artifacts/${base}.pdf"
+    npx -p @mermaid-js/mermaid-cli mmdc -p puppeteer-config.json -e pdf -i $full_name -o $parsed &&
     cd /tmp/docs
     pandoc -f markdown -o $out $parsed
-    rm./*
     cd ${wd}
   fi
 done
